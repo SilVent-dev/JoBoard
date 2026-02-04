7b5137b57ab6bbf391066d9822902508cccba9c6
Pedro Ventura <ventura.pdro@gmail.com>
2026-02-04 13:16:32 -0300

    Initial commit

---
+++ src/main/java/br/com/joboard/JoboardApplication.java
@@ -0,0 +1,13 @@
+package br.com.joboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joboard/JoboardApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.joboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//further implementation
+	}
+
+}
